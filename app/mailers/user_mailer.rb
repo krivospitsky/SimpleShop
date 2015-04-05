db62ac10198ebb1fd6ab1558a780ea7d06063465
alex krivospitsky <alex@krivospitsky.ru>
2015-04-05 13:03:56 +0300

    fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,6 +14,18 @@ class UserMailer < ActionMailer::Base
     }
   end
 
+  def delivery_options2
+    {
+      address:              Settings.smtp_host,
+      port:                 Settings.smtp_port,
+      domain:               Settings.smtp_domain,
+      # user_name:            Settings.smtp_user_name,
+      # password:             Settings.smtp_password,
+      # authentication:       Settings.smtp_authentication,
+      enable_starttls_auto: true
+    }
+  end
+
   def order_confirmation(order)
     @order=order
     mail(to: order.email, subject:"Подтверждение заказа", from: Settings.owner_email, delivery_method_options: delivery_options)
@@ -22,9 +34,9 @@ class UserMailer < ActionMailer::Base
   def new_order(order)
     @order=order
     if order.email.empty?
-    	mail(to: Settings.owner_email, subject:"Новый заказ", from: Settings.owner_email, delivery_method_options: delivery_options)
+    	mail(to: Settings.owner_email, subject:"Новый заказ", from: Settings.owner_email, delivery_method_options: delivery_options2)
     else
-    	mail(to: Settings.owner_email, subject:"Новый заказ", from: order.email, delivery_method_options: delivery_options)
+    	mail(to: Settings.owner_email, subject:"Новый заказ", from: order.email, delivery_method_options: delivery_options2)
     end
   end
 end
