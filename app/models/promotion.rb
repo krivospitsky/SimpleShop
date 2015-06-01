adead8fe304aca6d9d7bf8e1d48591d2f04b06ac
alex krivospitsky <alex@krivospitsky.ru>
2015-06-01 14:05:53 +0300

    можно не указывать даты в промоушенах

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -6,7 +6,9 @@ class Promotion < ActiveRecord::Base
   mount_uploader :banner
 
   scope :enabled, -> { where(enabled: 't') }
-  scope :current, -> { enabled.where('start_at< ? or end_at> ?', Time.now, Time.now) }
+#  scope :current, -> { enabled.where('start_at< ? or end_at> ?', Time.now, Time.now) }
+  scope :current, -> { enabled.where('(start_at< now() and end_at> now()) or (start_at< now() and end_at is NULL) or (start_at is NULL and end_at< now()) or (start_at is NULL and end_at is NULL)') }
+
   scope :current_banners, -> {enabled.where(has_banner: 't')}
 
 end
