89e6119f5f83fe1070d3da428193f2bc1fe9c3d7
alex krivospitsky <alex@krivospitsky.ru>
2015-12-01 22:23:27 +0300

    fixes

--- app/controllers/admin/commerce_ml_controller.rb
+++ app/controllers/admin/commerce_ml_controller.rb
@@ -10,6 +10,8 @@ skip_before_filter :verify_authenticity_token
 		if params[:mode]=='init'
 			dir='public/uploads/commerceml'
 			FileUtils.mkdir_p(dir) unless File.directory?(dir)
+			FileUtils.rm_f(File.join(dir, 'import.xml'))
+			FileUtils.rm_f(File.join(dir, 'import.xml'))
 			# FileUtils.rm_rf(dir)
 			render plain: "zip=no\nfile_limit=10670080"
 		end
@@ -18,7 +20,7 @@ skip_before_filter :verify_authenticity_token
 		    name = params[:filename]
     		dir = "public/uploads/commerceml"
     		path = File.join(dir, name)
-    		File.open(path, "wb") { |f| f.write(request.raw_post) }
+    		File.open(path, "ab") { |f| f.write(request.raw_post) }
 			render plain: "success"
 		end
 
