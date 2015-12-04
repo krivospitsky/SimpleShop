17e3c3a7a8969022c4d2457ef062e2186abccd23
alex krivospitsky <alex@krivospitsky.ru>
2015-12-04 18:57:23 +0300

    fixes

--- app/jobs/recreate_images_job.rb
+++ app/jobs/recreate_images_job.rb
@@ -2,6 +2,6 @@ class RecreateImagesJob < ActiveJob::Base
   queue_as :default
 
   def perform()
-    Category.find(1871).products.all.each{|p| p.images.all.each{|i| i.image.recreate_versions!}}
+    Product.all.each{|p| p.images.all.each{|i| i.image.recreate_versions!}}
   end
 end
