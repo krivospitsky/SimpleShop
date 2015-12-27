6644cf1abf8ad6c24d61be9d76f660dafd34574b
alex krivospitsky <alex@krivospitsky.ru>
2015-12-28 01:55:01 +0300

    fixes

---
+++ app/models/promotion.rb
@@ -0,0 +1,11 @@
+class Promotion < ActiveRecord::Base
+  include  Seoable
+  has_and_belongs_to_many(:categories)
+
+  has_and_belongs_to_many(:products)
+
+  scope :enabled, -> { where(enabled: 't') }
+#  scope :current, -> { enabled.where('start_at< ? or end_at> ?', Time.now, Time.now) }
+  scope :current, -> { enabled.where('(start_at< now() and end_at> now()) or (start_at< now() and end_at is NULL) or (start_at is NULL and end_at< now()) or (start_at is NULL and end_at is NULL)') }
+
+end
