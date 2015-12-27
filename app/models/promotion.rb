bf7deccfbd062de6636c6dd776034be25f1e62da
alex krivospitsky <alex@krivospitsky.ru>
2015-12-28 01:59:52 +0300

    fixes

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -4,6 +4,8 @@ class Promotion < ActiveRecord::Base
 
   has_and_belongs_to_many(:products)
 
+  mount_uploader :banner
+
   scope :enabled, -> { where(enabled: 't') }
 #  scope :current, -> { enabled.where('start_at< ? or end_at> ?', Time.now, Time.now) }
   scope :current, -> { enabled.where('(start_at< now() and end_at> now()) or (start_at< now() and end_at is NULL) or (start_at is NULL and end_at< now()) or (start_at is NULL and end_at is NULL)') }
