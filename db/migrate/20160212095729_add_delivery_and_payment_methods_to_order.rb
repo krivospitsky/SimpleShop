0415172d6667291e18175f5e03579b438163a65f
alex krivospitsky <alex@krivospitsky.ru>
2016-02-24 12:34:50 +0300

    fixes

--- db/migrate/20160212095729_add_delivery_and_payment_methods_to_order.rb
+++ db/migrate/20160212095729_add_delivery_and_payment_methods_to_order.rb
@@ -1,6 +1,6 @@
 class AddDeliveryAndPaymentMethodsToOrder < ActiveRecord::Migration
   def change
-    add_column :orders, :delivery_method, :integer
-    add_column :orders, :payment_method, :integer
+    add_column :orders, :delivery_method_id, :integer
+    add_column :orders, :payment_method_id, :integer
   end
 end
