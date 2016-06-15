774e4c844953cca49306970dfe31f5a387bf49de
alex krivospitsky <alex@krivospitsky.ru>
2016-06-15 15:03:33 +0300

    fixes

--- app/jobs/import_commerceml_job.rb
+++ app/jobs/import_commerceml_job.rb
@@ -2,9 +2,11 @@ class ImportCommercemlJob < ActiveJob::Base
   queue_as :default
 
   def perform(name)
+	# return if Settings.theme == 'fish' && name =='import.xml'
+	
 	dir = "public/uploads/commerceml"
 	path = File.join(dir, name)
-	
+		
 	doc = File.open(path) { |f| Nokogiri::XML(f) }
 
 	doc.xpath('КоммерческаяИнформация/Классификатор/Группы/Группа').each do |group|
@@ -12,81 +14,116 @@ class ImportCommercemlJob < ActiveJob::Base
 	end
 
 	doc.xpath('КоммерческаяИнформация/Каталог/Товары/Товар').each do |prod|    			
-		product=Product.find_or_initialize_by(external_id: prod.xpath('Ид').first.content.strip)
-		product.sku=prod.xpath('Артикул').first.content.strip
-		product.name=prod.xpath('Наименование').first.content.strip
 
-		puts product.name
+		if Settings.theme == 'fish'
+			variant=Variant.find_by(external_id: var.xpath('Ид').first.content.strip)	
+			variant|=Variant.find_by(sku: var.xpath('Штрихкод').first.content.strip)
+			variant|=Variant.find_by(sku: var.xpath('Артикул').first.content.strip)
 
-		prod.xpath('ЗначенияРеквизитов/ЗначениеРеквизита').each do |rec|    			
-			if rec.xpath('Наименование').first.content == 'Полное наименование'
-				product.description = rec.xpath('Значение').first.content.strip.gsub("\n", "<br>\n")
+			if variant
+				variant.external_id=var.xpath('Ид').first.content.strip
+				var.save
+			else
+				puts "variant #{var.xpath('Штрихкод').first.content.strip} - var.xpath('Артикул').first.content.strip not found"
 			end
-		end
+			
+		else
 
-		product.categories.delete_all
-        prod.xpath('Группы/Ид').each do |cat|
-        	product.categories << Category.find_by(external_id: cat.content.strip)
-        end
-        product.save
+			product=Product.find_or_initialize_by(external_id: prod.xpath('Ид').first.content.strip)
+			product.sku=prod.xpath('Артикул').first.content.strip
+			product.name=prod.xpath('Наименование').first.content.strip
+
+			puts product.name
+
+			prod.xpath('ЗначенияРеквизитов/ЗначениеРеквизита').each do |rec|    			
+				if rec.xpath('Наименование').first.content == 'Полное наименование'
+					product.description = rec.xpath('Значение').first.content.strip.gsub("\n", "<br>\n")
+				end
+			end
+
+			product.categories.delete_all
+	        prod.xpath('Группы/Ид').each do |cat|
+	        	product.categories << Category.find_by(external_id: cat.content.strip)
+	        end
+	        product.save
+	    end
 	end
 
 	doc.xpath('КоммерческаяИнформация/ПакетПредложений/Предложения/Предложение').each do |var|    			
-		variant=Variant.find_or_initialize_by(external_id: var.xpath('Ид').first.content.strip)
-		variant.product=Product.find_by(external_id: variant.external_id.split('#')[0])
-		var.xpath('Цены/Цена').each do |price|
-			if price.xpath('ИдТипаЦены').first.content.strip == 'cbcf493b-55bc-11d9-848a-00112f43529a'
-				variant.price=price.xpath('ЦенаЗаЕдиницу').first.content.strip
-			end
-		end
-		variant.count=var.xpath('Количество').first.content.strip
-		if variant.count>0
-			variant.availability='В наличии'
-			variant.enabled=true
-	        product=variant.product
-	        # puts "!!! #{product.name} !! #{product.enabled}"
-	        if product.enabled != true
-		        puts "enabling  #{product.name}"
-	        	product.enabled=true
-	        	product.returned_at=Time.now()
-	        	product.save
-	        end
+		if Settings.theme == 'fish'
+			variant=Variant.find_by(external_id: var.xpath('Ид').first.content.strip)
+			if variant
+				variant.availability='В наличии'
+				variant.enabled=true
+				product=variant.product
 
+		        if product.enabled != true
+			        puts "enabling  #{product.name}"
+		        	product.enabled=true
+		        	product.returned_at=Time.now()
+		        	product.save
+		        end
+		        variant.save
+		    else
+		    	puts "variant #{var.xpath('Ид').first.content.strip} not found"
+		    end
 		else
-			variant.availability='Нет в наличии'
-			variant.enabled=false
-		end
+			variant=Variant.find_or_initialize_by(external_id: var.xpath('Ид').first.content.strip)
+			variant.product=Product.find_by(external_id: variant.external_id.split('#')[0])
+			var.xpath('Цены/Цена').each do |price|
+				if price.xpath('ИдТипаЦены').first.content.strip == 'cbcf493b-55bc-11d9-848a-00112f43529a'
+					variant.price=price.xpath('ЦенаЗаЕдиницу').first.content.strip
+				end
+			end
+			variant.count=var.xpath('Количество').first.content.strip
+			if variant.count>0
+				variant.availability='В наличии'
+				variant.enabled=true
+		        product=variant.product
+		        # puts "!!! #{product.name} !! #{product.enabled}"
+		        if product.enabled != true
+			        puts "enabling  #{product.name}"
+		        	product.enabled=true
+		        	product.returned_at=Time.now()
+		        	product.save
+		        end
 
-		variant.name=variant.product.name    			
-		variant.sku=variant.product.sku
-		variant.attrs.delete_all
-		first_var=true
-		var_exist=false
-		var.xpath('ХарактеристикиТовара/ХарактеристикаТовара').each do |v_attr|
-			if first_var
-				variant.name+=' ('
 			else
-				variant.name+=', '    					
+				variant.availability='Нет в наличии'
+				variant.enabled=false
 			end
-			a=variant.attrs.new
-			a.name=v_attr.xpath('Наименование').first.content.strip
-			a.value=v_attr.xpath('Значение').first.content.strip
-			if a.value.match(/\((.+)\)/)
-				puts "#{a.value} -> #{Regexp.last_match[1]}"
-				a.value=Regexp.last_match[1]
+
+			variant.name=variant.product.name    			
+			variant.sku=variant.product.sku
+			variant.attrs.delete_all
+			first_var=true
+			var_exist=false
+			var.xpath('ХарактеристикиТовара/ХарактеристикаТовара').each do |v_attr|
+				if first_var
+					variant.name+=' ('
+				else
+					variant.name+=', '    					
+				end
+				a=variant.attrs.new
+				a.name=v_attr.xpath('Наименование').first.content.strip
+				a.value=v_attr.xpath('Значение').first.content.strip
+				if a.value.match(/\((.+)\)/)
+					puts "#{a.value} -> #{Regexp.last_match[1]}"
+					a.value=Regexp.last_match[1]
+				end
+				a.save
+				variant.name+= "#{a.name.mb_chars.downcase.to_s} #{a.value}"
+				variant.sku+="_#{a.value}"
+				first_var=false
+				var_exist=true
 			end
-			a.save
-			variant.name+= "#{a.name.mb_chars.downcase.to_s} #{a.value}"
-			variant.sku+="_#{a.value}"
-			first_var=false
-			var_exist=true
+			variant.name+=')' if var_exist
+			puts variant.name
+			variant.save    			
 		end
-		variant.name+=')' if var_exist
-		puts variant.name
-		variant.save    			
 	end
 
-	if name =='offers.xml'
+	if name =='offers.xml' && Settings.theme != 'fish'
 		# Variant.where("updated_at < ? and updated_at >= ?", 1.day.ago, 2.week.ago, like_str).update_all(availability: 'Уточнить у менеджера')
 		Product.all.each do |prod|
 			if prod.variants.enabled.empty?
@@ -95,8 +132,10 @@ class ImportCommercemlJob < ActiveJob::Base
 				prod.save
 			end
 		end		
-		`rake -f #{Rails.root.join("Rakefile")} moysklad:import_photos`
-		`rake -f #{Rails.root.join("Rakefile")} moysklad:import_users`
+		if Settings.theme == 'mama40'
+			`rake -f #{Rails.root.join("Rakefile")} moysklad:import_photos`
+			`rake -f #{Rails.root.join("Rakefile")} moysklad:import_users`
+		end
 	end
   end
 
